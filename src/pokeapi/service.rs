4015fdd6fa4ecce24f60903cf2b508a51631f4af
angelocat <catalaniangelo@gmail.com>
2021-07-24 19:33:53 +0200

    test(pokemon): pokeapi service parse correctly valid response

--- src/pokeapi/service.rs
+++ src/pokeapi/service.rs
@@ -51,3 +51,87 @@ impl PokeapiService {
         Ok(graphql_response)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::convert::TryFrom;
+
+    use reqwest::{StatusCode, Url};
+    use serde::Serialize;
+    use serde_json::{json, Value};
+    use wiremock::matchers::body_json;
+    use wiremock::matchers::{header, method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn pokeapi_service_successfully_parses_valid_response() {
+        let server = MockServer::start().await;
+
+        let pokemon_name = "mewtwo";
+        let habitat = "rare";
+        let description = "It was created by a scientist";
+        let is_legendary = true;
+
+        Mock::given(method("POST"))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_json(build_pokeapi_response(
+                    pokemon_name,
+                    habitat,
+                    description,
+                    is_legendary,
+                )),
+            )
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+
+        let correct_response = json!({
+            "name": pokemon_name,
+            "habitat": habitat,
+            "description":description,
+            "isLegendary":is_legendary
+
+        });
+
+        assert_eq!(
+            correct_response,
+            json!(service
+                .get_pokemon(pokemon_name.to_string())
+                .await
+                .unwrap()
+                .unwrap())
+        );
+    }
+
+    fn build_pokeapi_response(
+        pokemon_name: &str,
+        habitat: &str,
+        flavor_text: &str,
+        is_legendary: bool,
+    ) -> Value {
+        json!(
+            {
+               "data":{
+                  "info":[
+                     {
+                        "name":pokemon_name,
+                        "habitat":{
+                           "name":habitat
+                        },
+                        "descriptions":[
+                           {
+                              "flavor_text":flavor_text
+                           }
+                        ],
+                        "is_legendary":is_legendary
+                     }
+                  ]
+               }
+            }
+        )
+    }
+}
