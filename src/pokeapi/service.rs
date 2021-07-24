70ba1f2cd38131635a9e559ebe66efd5181723b9
angelocat <catalaniangelo@gmail.com>
2021-07-24 23:12:46 +0200

    test(pokemon): pokeapi service handles correctly error responses

--- src/pokeapi/service.rs
+++ src/pokeapi/service.rs
@@ -54,19 +54,15 @@ impl PokeapiService {
 
 #[cfg(test)]
 mod tests {
-    use std::convert::TryFrom;
-
-    use reqwest::{StatusCode, Url};
-    use serde::Serialize;
+    use reqwest::StatusCode;
     use serde_json::{json, Value};
-    use wiremock::matchers::body_json;
-    use wiremock::matchers::{header, method, path};
+    use wiremock::matchers::method;
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
     use super::*;
 
     #[tokio::test]
-    async fn pokeapi_service_successfully_parses_valid_response() {
+    async fn pokeapi_service_successfully_parses_response_with_all_fields() {
         let server = MockServer::start().await;
 
         let pokemon_name = "mewtwo";
@@ -78,8 +74,8 @@ mod tests {
             .respond_with(
                 ResponseTemplate::new(200).set_body_json(build_pokeapi_response(
                     pokemon_name,
-                    habitat,
-                    description,
+                    Some(habitat),
+                    &[description],
                     is_legendary,
                 )),
             )
@@ -107,31 +103,175 @@ mod tests {
         );
     }
 
+    #[tokio::test]
+    async fn pokeapi_service_successfully_parses_response_with_missing_fields() {
+        let server = MockServer::start().await;
+
+        let pokemon_name = "mewtwo";
+        let is_legendary = true;
+
+        Mock::given(method("POST"))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_json(build_pokeapi_response(
+                    pokemon_name,
+                    None,
+                    &[],
+                    is_legendary,
+                )),
+            )
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+
+        let correct_response = json!({
+            "name": pokemon_name,
+            "isLegendary":is_legendary
+
+        });
+
+        assert_eq!(
+            correct_response,
+            json!(service
+                .get_pokemon(pokemon_name.to_string())
+                .await
+                .unwrap()
+                .unwrap())
+        );
+    }
+
+    #[tokio::test]
+    async fn pokeapi_service_handles_correctly_empty_response_without_pokemons() {
+        let server = MockServer::start().await;
+
+        Mock::given(method("POST"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!(
+                {
+                    "data":{
+                        "info":[]
+                    }
+                }
+            )))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+
+        assert!(service
+            .get_pokemon("any_pokemon".into())
+            .await
+            .unwrap()
+            .is_err());
+    }
+
+    #[tokio::test]
+    async fn pokeapi_service_handles_correctly_error_response() {
+        let server = MockServer::start().await;
+
+        Mock::given(method("POST"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!(
+                {
+                    "error": [
+                        "pokeapi_internal_error"
+                    ]
+                }
+            )))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+
+        assert!(service
+            .get_pokemon("any_pokemon".into())
+            .await
+            .unwrap()
+            .is_err());
+    }
+
+    #[tokio::test]
+    async fn pokeapi_service_handles_correctly_unexpected_fields_inside_response() {
+        let server = MockServer::start().await;
+
+        Mock::given(method("POST"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!(
+                {
+                    "unexpected_field": "unexpected_value"
+                }
+            )))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+        assert!(service
+            .get_pokemon("any_pokemon".into())
+            .await
+            .unwrap()
+            .is_err());
+    }
+
+    #[tokio::test]
+    async fn pokeapi_service_handles_correctly_http_error_request() {
+        let server = MockServer::start().await;
+
+        Mock::given(method("POST"))
+            .respond_with(ResponseTemplate::new(StatusCode::BAD_REQUEST))
+            .expect(1)
+            .mount(&server)
+            .await;
+
+        let service = PokeapiService::new(server.uri().parse().unwrap()).unwrap();
+        assert!(service.get_pokemon("any_pokemon".into()).await.is_err());
+    }
+
     fn build_pokeapi_response(
         pokemon_name: &str,
-        habitat: &str,
-        flavor_text: &str,
+        habitat: Option<&str>,
+        descriptions: &[&str],
         is_legendary: bool,
     ) -> Value {
-        json!(
-            {
-               "data":{
-                  "info":[
-                     {
-                        "name":pokemon_name,
-                        "habitat":{
-                           "name":habitat
-                        },
-                        "descriptions":[
-                           {
-                              "flavor_text":flavor_text
-                           }
-                        ],
-                        "is_legendary":is_legendary
-                     }
-                  ]
-               }
+        let flavor_texts = descriptions
+            .iter()
+            .map(|d| json!({ "flavor_text": d }))
+            .collect::<Vec<Value>>();
+
+        match habitat {
+            Some(habitat_name) => {
+                json!(
+                    {
+                       "data":{
+                          "info":[
+                             {
+                                "name":pokemon_name,
+                                "habitat":{
+                                   "name":habitat_name
+                                },
+                                 "descriptions": flavor_texts,
+                                "is_legendary":is_legendary
+                             }
+                          ]
+                       }
+                    }
+                )
             }
-        )
+            None => {
+                json!(
+                    {
+                       "data":{
+                          "info":[
+                             {
+                                "name":pokemon_name,
+                                 "descriptions": flavor_texts,
+                                "is_legendary":is_legendary
+                             }
+                          ]
+                       }
+                    }
+                )
+            }
+        }
     }
 }
